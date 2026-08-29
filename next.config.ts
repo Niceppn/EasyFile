86bdb5ba76a3bf55cdddedb7e6d852d75bfca81e
unknown <PPhutana01@gmail.com>
2026-08-29 23:13:20 +0700

    Speed up Docker builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  typescript: {
+    // Speed up Docker builds on 1 vCPU servers (since local build already type-checks)
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   turbopack: {},
   webpack: (config) => {
     config.resolve.alias.canvas = false;
